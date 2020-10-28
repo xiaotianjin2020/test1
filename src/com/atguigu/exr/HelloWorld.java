5c03797252a305d9f3be0fe48f5d79803d18afd0
feiji <xiaotianjin1988@163.com>
2020-10-28 22:42:08 +0800

    day05

---
+++ src/com/atguigu/exr/HelloWorld.java
@@ -0,0 +1,14 @@
+package com.atguigu.exr;
+
+/**
+ * @author shkstart
+ * @create 2020-06-29 10:58
+ */
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("HelloWOld");
+
+    }
+
+
+}
